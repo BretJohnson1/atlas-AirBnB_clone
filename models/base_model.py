5d92f5a5ec0a0e3eb5a57eb9f70689d98fd9b0c0
Trey Turner <trey.turner1857@gmail.com>
2024-05-30 13:54:48 -0500

    Updated base_models.py to work for task 4. Created the unittests for task 3.

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,34 @@
 from datetime import datetime
 import uuid
 
-
 """Defines a class BaseModel"""
 
 
 class BaseModel:
     """Class that defines all common attributes for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instantiation of BaseModel"""
+        from models import storage
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                storage.new(self)
 
     def __str__(self):
         """Returns the string representation"""
---
+++ tests/test_models/test_base_models.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+"""Defines a class TestBaseModel"""
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        """Set up a new BaseModel instance for each test."""
+        self.model = BaseModel()
+
+    def test_save(self):
+        """Test the save()"""
+        original_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(original_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        """Test the to_dict()"""
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn("__class__", model_dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+
+    def test_id(self):
+        """Test the self.id"""
+        self.assertIsInstance(self.model.id, str)
+        self.assertTrue(len(self.model.id) > 0)
+
+    def test_created_at(self):
+        """Test the self.created_at"""
+        self.assertIsInstance(self.model.created_at, datetime.datetime)
+
+    def test_str(self):
+        """Test the __str__(self)"""
+        self.assertEqual(str(self.model), "[BaseModel] ({})\
+ {}".format(self.model.id, self.model.__dict__))
+
+if __name__ == '__main__':
+    unittest.main()
