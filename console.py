ddc3dd7e3361a6b21b5580ba6fdd2c84b21dc4bc
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-05-31 13:20:04 -0500

    Added quit and exit methods to console

--- console.py
+++ console.py
@@ -30,4 +30,30 @@ class HBNBCommand(cmd.Cmd):
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
         'first_name': str, 'number_rooms': int, 'number_bathrooms': int, 'max_guest': int, 'price_by_night': int, 'email': str, 'latitude': float, 'longitude': float, 'last_name': str, 'password': str
-            }
+    }
+
+    def preloop(self):
+        """ Prints if isatty is false """
+        if not sys.__stdin__.isatty():
+            print('(hbnb)')
+
+    def postcmd(self, stop, line):
+        """ Prints if isatty is false """
+        if not sys.__stdin__.isatty():
+            print('(hbnb) ', end='')
+            return stop
+
+    def do_quit(self, command):
+        """ Method to exit the HBNB console """
+        exit()
+
+    def help_quit(self):
+        """ Prints the help documentation for quit """
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        exit()
+
+    
