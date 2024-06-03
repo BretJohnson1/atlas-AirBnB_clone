4309dec0c18d69a8a4b40128a347e513589411ea
Trey Turner <trey.turner1857@gmail.com>
2024-06-03 18:45:41 -0500

    Trailing whitespace fix

--- console.py
+++ console.py
@@ -29,14 +29,14 @@ class HBNBCommand(cmd.Cmd):
     }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-        'first_name': str, 
-        'number_rooms': int, 
+        'first_name': str,
+        'number_rooms': int,
         'number_bathrooms': int,
-        'max_guest': int, 
-        'price_by_night': int, 
+        'max_guest': int,
+        'price_by_night': int,
         'email': str,
-        'latitude': float, 
-        'longitude': float, 
+        'latitude': float,
+        'longitude': float,
         'last_name': str,
         'password': str
     }
