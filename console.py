e2ee159424f419e2d179c711f23731a5085e32cc
Trey Turner <trey.turner1857@gmail.com>
2024-06-03 18:42:32 -0500

    Decided to align vertically

--- console.py
+++ console.py
@@ -29,10 +29,16 @@ class HBNBCommand(cmd.Cmd):
     }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-        'first_name': str, 'number_rooms': int, 'number_bathrooms': int,
-         'max_guest': int, 'price_by_night': int, 'email': str,
-         'latitude': float, 'longitude': float, 'last_name': str,
-         'password': str
+        'first_name': str, 
+        'number_rooms': int, 
+        'number_bathrooms': int,
+        'max_guest': int, 
+        'price_by_night': int, 
+        'email': str,
+        'latitude': float, 
+        'longitude': float, 
+        'last_name': str,
+        'password': str
     }
 
     def preloop(self):
