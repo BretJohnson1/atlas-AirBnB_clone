330469d68e71ae215227c3920364dcc01c602ddb
Trey Turner <trey.turner1857@gmail.com>
2024-06-03 18:28:32 -0500

    fixing indentation

--- models/base_model.py
+++ models/base_model.py
@@ -43,16 +43,15 @@ class BaseModel:
  {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-          """Updates the public instantance attr with current date/time"""
-          from models import storage
-          self.updated_at = datetime.now()
-          storage.save()
-
+        """Updates the public instantance attr with current date/time"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-          """Returns a dictionary of with all key/values of dict"""
-          dict_copy = self.__dict__.copy()
-          dict_copy["__class__"] = self.__class__.__name__
-          dict_copy["created_at"] = self.created_at.isoformat()
-          dict_copy["updated_at"] = self.updated_at.isoformat()
-          return dict_copy
+        """Returns a dictionary of with all key/values of dict"""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
