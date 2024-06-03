a99dc1abb3301e393b60db99f92bce4cada63938
Trey Turner <trey.turner1857@gmail.com>
2024-06-03 18:39:55 -0500

    indentation fix

--- console.py
+++ console.py
@@ -30,9 +30,9 @@ class HBNBCommand(cmd.Cmd):
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
         'first_name': str, 'number_rooms': int, 'number_bathrooms': int,
- 'max_guest': int, 'price_by_night': int, 'email': str,
- 'latitude': float, 'longitude': float, 'last_name': str,
- 'password': str
+         'max_guest': int, 'price_by_night': int, 'email': str,
+         'latitude': float, 'longitude': float, 'last_name': str,
+         'password': str
     }
 
     def preloop(self):
